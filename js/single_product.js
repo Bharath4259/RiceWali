37f99284c97da7366cb48b661f0f1701a5141ae0
BHarath R <bharath.r@gramener.com>
2019-01-20 14:24:26 +0530

    updated project

--- js/single_product.js
+++ js/single_product.js
@@ -1,36 +1,45 @@
 var data= []
+var product_category = window.location.search.substr(1)
 $( document ).ready(function() {
     console.log( "-- ready --> ", render_data );
-    data = render_data[0][window.location.search.substr(1)]
-    console.log("--> loc: ", window.location.search.substr(1))
+    data = render_data[0][product_category]
+    console.log("--> loc: ", product_category)
     console.log("-- data --> ", data)
     render(data);
+
+    $(".dropdown").hover(
+        function () {
+            $('.dropdown-menu', this).stop(true, true).slideDown("fast");
+            $(this).toggleClass('open');
+        },
+        function () {
+            $('.dropdown-menu', this).stop(true, true).slideUp("fast");
+            $(this).toggleClass('open');
+        }
+    );
+    // here starts scrolling icon
+	$().UItoTop({ easingType: 'easeOutQuart' });
 });
+
+$(document).ready(function () {
+    d3.csv("static/assets/products_data.csv", function(data){
+        console.log("d3data > ", data);
+        $.map(data, function(item){
+            if(item.prod_category === product_category)
+                render_product(item)
+        })
+    });
+});
+
 function render(product_data){
     $("#p_title").text(product_data.title)
     $("#p_img").attr("src", product_data.image)
     $("#p_desc").text(product_data.desc)
-    $("#p_price").html("₹" + product_data.price + " <span> ₹" + product_data.cprice + " </span>")
+    $("#p_price").html("Avg price on our site &nbsp; ₹" + product_data.price )
+    $("#p_mprice").html("Avg price in Market &nbsp;&nbsp;&nbsp; ₹" + product_data.mprice )
     $("#p_abt_ttle").text("More About " + product_data.title)
     $.each(product_data.specs, function(i, v){
         $("#p_specs").append('<li><i class="fa fa-check" aria-hidden="true"></i><a>'+ v +'</a></li>')
     })
 }
 
-
-/*
-     "basmati": {
-        "title": "basmati",
-        "image": "images/76.png",
-        "desc": "Basmati is a variety of long, slender-grained aromatic rice which is traditionally from the Indian subcontinent. These long and slender grains are known for their distinct aroma and are native to the Indian subcontinent. It is commonly used for making royal biryanis but there's a lot more that you can do with Basmati rice. If you haven't experimented with it yet, here are our 10 best Basmati rice recipes to try.",
-        "price": "1500",
-        "cprice": "1800",
-        "specs": ["SUITABLE FOR BIRYANI",
-                    "HIGH CALORIFIC VALUE",
-                    "CARBOHYDRATES AND PROTIENS",
-                    "LOWER IN FAT AND GLUTEN",
-                    "RICH SOURCE OF VITAMINS",
-                    "IRON IS ANOTHER IMPORTANT NUTRIENT THAT BASMATI RICE ADDS TO ONE’S DIET"
-                ]
-    }
-*/
--- render.js
+++ render.js
@@ -1,10 +1,10 @@
 render_data = [{
     "basmati": {
         "title": "basmati",
-        "image": "images/76.png",
+        "image": "static/assets/images/76.png",
         "desc": "Basmati is a variety of long, slender-grained aromatic rice which is traditionally from the Indian subcontinent. These long and slender grains are known for their distinct aroma and are native to the Indian subcontinent. It is commonly used for making royal biryanis but there's a lot more that you can do with Basmati rice. If you haven't experimented with it yet, here are our 10 best Basmati rice recipes to try.",
         "price": "1500",
-        "cprice": "1800",
+        "mprice": "1800",
         "specs": ["SUITABLE FOR BIRYANI",
                     "HIGH CALORIFIC VALUE",
                     "CARBOHYDRATES AND PROTIENS",
